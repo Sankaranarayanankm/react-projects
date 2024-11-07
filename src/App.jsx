3feceb2a37c70075a3bbaa66e002569de611376f
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2024-11-07 16:44:06 +0530

    load more project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import RandomColor from "./Components/Color-picker/RandomColor/RandomColor";
 import StarRating from "./Components/Star-rating/index.jsx";
 import ImageSlider from "./Components/ImageSlider/ImageSlider.jsx";
+import LoadMore from "./Components/LoadMore/LoadMore.jsx";
 
 const App = () => {
   return (
@@ -14,7 +15,10 @@ const App = () => {
       {/* <StarRating noOfStars={8} /> */}
 
       {/* image slider project  */}
-      <ImageSlider />
+      {/* <ImageSlider /> */}
+
+      {/* load more project  */}
+      <LoadMore />
     </div>
   );
 };
---
+++ src/Components/LoadMore/LoadMore.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LoadMore.css";
+
+const LoadMore = () => {
+  return <div className="loadMore">LoadMore</div>;
+};
+
+export default LoadMore;
