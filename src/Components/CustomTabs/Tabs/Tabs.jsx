21df22d956d8f2c4a7a929c8692f7e0b73eb4ba6
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2024-11-10 10:27:24 +0530

    completed tabs project

--- src/Components/CustomTabs/Tabs/Tabs.jsx
+++ src/Components/CustomTabs/Tabs/Tabs.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Tabs.css";
-const Tabs = ({ tabsContent, onChange }) => {
-  return <div className="tabs">Tabs</div>;
+const Tabs = ({ tabsContent }) => {
+  const [currentItem, setCurrentItem] = useState(tabsContent[0] || "");
+  function handleClick(id) {
+    const current = tabsContent.find((item) => item.id == id);
+
+    setCurrentItem(current);
+  }
+  return (
+    <div className="tabs">
+      <ul className="tabs__heading">
+        {tabsContent.map((item) => (
+          <li
+            className={`tabs__item ${
+              currentItem.id == item.id ? "active" : undefined
+            }`}
+            key={item.id}
+            onClick={() => handleClick(item.id)}
+          >
+            {item.label}
+          </li>
+        ))}
+      </ul>
+      <div className="tabs__content">
+        <span>{currentItem.content}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Tabs;
--- src/Components/CustomTabs/TabsTest.jsx
+++ src/Components/CustomTabs/TabsTest.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Tabs from "./Tabs/Tabs";
 
 const TabsTest = () => {
-  return (
-    <div>
-      <Tabs />
-    </div>
-  );
+  const list = [
+    { id: 1, label: "Tab1", content: "This is the content for Tab 1." },
+    { id: 2, label: "Tab2", content: "This is the content for Tab 2." },
+    { id: 3, label: "Tab3", content: "This is the content for Tab 3." },
+    { id: 4, label: "Tab4", content: "This is the content for Tab 4." },
+    { id: 5, label: "Tab5", content: "This is the content for Tab 5." },
+  ];
+
+  return <Tabs tabsContent={list} />;
 };
 
 export default TabsTest;
